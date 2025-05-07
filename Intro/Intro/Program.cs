4c3f690fb30a53eeb0fe0dfe817f85a81ff05c8f
wayne <profbat018@gmail.com>
2025-05-07 11:55:01 +0400

    Updated

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Test obj = default;
+
+if (obj == null)
+{
+    Console.WriteLine("obj is null");
+}
+else
+{
+    Console.WriteLine("obj is not null");
+}
+
+class Test
+{
+    public int a;
+
+    public Test()
+    {
+        a = 5;
+    }
+}
+
