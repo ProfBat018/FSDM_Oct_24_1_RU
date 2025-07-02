47b68c7cd70e1682edb1a1078c15b86f7319c718
Azimov Elvin Rasul <azimove@itstep.edu.az>
2025-07-02 09:09:48 +0400

    Updated

---
+++ Lesson1/Lesson1.sql
@@ -0,0 +1,40 @@
+use Showroom;
+
+-- DDL
+CREATE TABLE Cars
+(
+    [Id] INT PRIMARY KEY IDENTITY (1, 1),
+    [Make] NVARCHAR(30),
+    [Model] NVARCHAR(30),
+    [Year] INT CHECK([Year] >= 1885 AND [Year] <= Year(GETUTCDATE()))
+);
+
+
+-- DML
+-- INSERT INTO Cars(Id, Make, Model) VALUES(1, N'Mercedes-Benz', N'S63 AMG');
+-- INSERT INTO Cars(Id, M```ake) VALUES(1, N'Mercedes-Benz');
+-- INSERT INTO Cars VALUES(1, N'Mercedes-Benz', N'S63 AMG');
+
+-- INSERT INTO Cars( Make, Model) VALUES( N'Mercedes-Benz', N'S63 AMG');
+-- INSERT INTO Cars( Make, Model) VALUES( N'Mercedes-Benz', N'E63 AMG');
+
+
+INSERT INTO Cars(Make, Model, Year) VALUES(N'Mercedes-Benz', N'E63 AMG', 2008);
+INSERT INTO Cars(Make, Model, Year) VALUES(N'Mercedes-Benz', N'E63 AMG', 2026);
+
+select * from Cars;
+
+
+select * from Cars
+where Make = 'toyota'
+
+
+select * from Cars
+where Make LIKE 'toyota'
+
+select * from Cars
+where Make LIKE 'm%'
+
+select * from Cars
+where Make LIKE '[A-C]%'
+
--- MovieApp/MovieApp/DTOs/Requests/LoginRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MovieApp.DTOs.Requests;
-
-public record LoginRequest(string Email, string Password);
